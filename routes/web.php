c58bf6f3a6bfe7a4104481cbd54bd54999ec9b42
untercoder <nosekpt@gmail.com>
2021-03-18 20:02:59 +0300

    Create project commit: Config: Laravel 8 React MySQl PHP 7.4
    Test git commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('git', function () {
+    return "<h1>Все четко git работает!</h1>";
+});
